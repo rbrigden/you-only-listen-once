4ad582245380ac576e48618305b6823bbe392e1b
Ryan Brigden <rbrigden@cmu.edu>
2019-02-18 12:26:25 -0500

    Add demo

--- inference/demo.py
+++ inference/demo.py
@@ -1,2 +1,104 @@
 import torch
-import numpy a
+import torch.nn as nn
+import numpy as np
+import training.speaker_classification.model as models
+import torch.utils.data as data
+import argparse
+import librosa
+import os
+from torch.nn.utils.rnn import pad_sequence
+
+
+class WavDataset(data.Dataset):
+
+    def __init__(self, source_path):
+        super(WavDataset, self).__init__()
+        file_names = os.listdir(source_path)
+
+        utterances = []
+        labels = []
+        for name in file_names:
+            wav_path = os.path.join(source_path, name)
+            utterances.append(process_with_mel(wav_path))
+            labels.append(os.path.splitext(name)[0])
+
+        self.utterances = utterances
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.utterances)
+
+    def __getitem__(self, item):
+        return [torch.FloatTensor(self.utterances[item]), self.labels[item]]
+
+
+def sample_normalize(x):
+    stats_path = "data/voxceleb/voxceleb_stats.npy"
+    mean, variance, _ = np.load(stats_path)
+    mean, variance = torch.FloatTensor(mean), torch.FloatTensor(variance)
+    return (x - mean.view(1, -1)) / torch.sqrt(variance.view(1, -1))
+
+
+def process_with_mel(wav_path):
+    y, sr = librosa.load(wav_path)
+    mel = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=2048, n_mels=64)
+    assert mel.shape[0] == 64
+    return mel
+
+
+def collate(batch):
+    data = [item[0].permute(1, 0) for item in batch]
+    data = [sample_normalize(x) for x in data]
+    target = [item[1] for item in batch]
+    return [data, target]
+
+
+class SpeakerEmbedInference:
+
+    def __init__(self, model):
+        self.model = model
+
+    def _process_data_batch(self, data_batch):
+        # pad the sequences, each seq must be (L, *)
+        seq_lens = [len(x) for x in data_batch]
+        seq_batch = pad_sequence(data_batch, batch_first=True)
+        return seq_batch.unsqueeze(1).cuda(), seq_lens
+
+    def forward(self, x):
+        self.model.eval()
+        seq_batch, seq_lens = self._process_data_batch(x)
+        with torch.no_grad():
+            embeddings = self.model([seq_batch, seq_lens], em=True)
+        return embeddings
+
+    def load_params(self, checkpoint_path):
+        cpd = torch.load(checkpoint_path, map_location=lambda storage, loc: storage)
+        self.model.load_state_dict(cpd["model"])
+
+
+def plot_embeddings(embeddings, labels):
+    raise NotImplementedError
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--source", type=str,
+                        default='/tmp/demo_utterances',
+                        help='directory with wavs')
+
+    args = parser.parse_args()
+    source_path = args.source
+
+    model = models.SpeakerClassifier2d(1200)
+
+    inference = SpeakerEmbedInference(model)
+
+    dset = WavDataset(source_path)
+
+    loader = data.DataLoader(dset, shuffle=False, batch_size=1, collate_fn=collate)
+
+    for utterances, labels in loader:
+        embeddings = inference.forward(utterances)
+
+        plot_embeddings(embeddings.numpy(), labels)
