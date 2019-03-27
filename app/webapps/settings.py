ee500c7823a3514659330e837e4c3cf639ae91a4
richaravi97 <richa.ravi@gmail.com>
2019-03-26 23:15:26 -0400

    using forms to send blob

--- app/webapps/settings.py
+++ app/webapps/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p--q!xxmw&fs#6s+@8&@mje!a+)36#7+dv!(k#!hc*owtm(4t7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
